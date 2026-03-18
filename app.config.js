c682e64441e3cf092b39ff151b494dbd3bc52c02
Aunray Stanford <aunray@outlook.com>
2026-03-18 01:27:06 -0400

    chore: bump iOS build number to 2

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ module.exports = {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.BOL.stattracker",
-      buildNumber: "1",
+      buildNumber: "2",
       infoPlist: {
         NSCameraUsageDescription:
           "This app uses the camera to capture team photos and player avatars.",
